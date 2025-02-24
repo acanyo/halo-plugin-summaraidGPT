6bd6575d18cfcba094392d13fbc9556452deec0b
acanyo <30819792@qq.com>
2025-02-24 23:52:28 +0800

    修改静态CDN地址

--- src/main/java/com/handsome/summary/Constant.java
+++ src/main/java/com/handsome/summary/Constant.java
@@ -13,12 +13,12 @@ public enum Constant {
     public static final String TOKEN_URL = "https://aip.baidubce.com/oauth/2.0/token";
 
     public static final String DEFAULT_CSS =
-        "https://cdn.jsdmirror.com/gh/acanyo/halo-aipost-summaraidGPT@1.0.0/ArticleSummary.css";
+        "https://www.lik.cc/upload/ArticleSummary.css";
 
     public static final String DEFAULT_ICON = "https://www.lik.cc/upload/icon.svg";
 
     public static final String scriptUrl =
-        "https://cdn.jsdmirror.com/gh/acanyo/halo-aipost-summaraidGPT@1.0.0/ArticleSummary.js";
+        "https://www.lik.cc/upload/ArticleSummary.js";
 
     public static final String TEMPLATE_ID_VARIABLE = "_templateId";
 
